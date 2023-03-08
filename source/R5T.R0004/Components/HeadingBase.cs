3f8796fc18303287deaf470694422e01c3e0cf0f
davidcoats <david@getsafetycone.com>
2023-03-07 23:51:39 -0800

    Interim changes.

--- source/R5T.R0004/Components/HeadingBase.cs
+++ source/R5T.R0004/Components/HeadingBase.cs
@@ -7,14 +7,31 @@ namespace R5T.R0004
 {
     public class HeadingBase : ComponentBase
     {
+        /// <summary>
+        /// Goes from 1 (h1) to 6 (h6).
+        /// </summary>
         [Parameter]
-        public string Value { get; set; }
+        public int Level { get; set; }
+
+        [Parameter]
+        public string TableOfContentsText { get; set; }
+
+        [Parameter]
+        public string HeadingText { get; set; }
+
+        [Parameter]
+        public string Value { set
+            {
+                this.TableOfContentsText = value;
+                this.HeadingText = value;
+            }
+        }
 
         public string Identity
         {
             get
             {
-                var identity = Instances.HtmlIdentityOperator.GetHeadingIdentity(this.Value);
+                var identity = Instances.HtmlIdentityOperator.GetHeadingIdentity(this.HeadingText);
                 return identity;
             }
         }
--- source/R5T.R0004/Project Plan.md
+++ source/R5T.R0004/Project Plan.md
@@ -1,2 +1,23 @@
 # R5T.R0004
 Table of contents Razor Components.
+
+
+Otherwise you would have to make static ToC HTML, and as you move sections around, the static HTML might get out of order with the actual HTML!
+
+
+## Example Usage
+
+
+    <TableOfContentsContext>
+        <HeadingL2 Value="How to see <meta> tags" Level="1"/>
+
+
+        <HeadingL2 Value="The Basics" Level="1" />
+
+
+        <HeadingL3 Value="<title>" Level="2" />
+
+
+        <HeadingL3 Value="Description-<meta>" Level="2" />
+
+    </TableOfContentsContext>
