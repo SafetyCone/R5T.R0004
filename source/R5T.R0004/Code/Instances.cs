921c6755081252512b12af315fe7915bafbe04e8
davidcoats <david@getsafetycone.com>
2025-07-25 12:25:11 -0700

    Interim commit.

--- source/R5T.R0004/Code/Instances.cs
+++ source/R5T.R0004/Code/Instances.cs
@@ -5,6 +5,7 @@ namespace R5T.R0004
 {
     public static class Instances
     {
+        public static F10Y.L0001.IGuidOperator GuidOperator => F10Y.L0001.GuidOperator.Instance;
         public static T0016.IHtmlIdentityOperator HtmlIdentityOperator => T0016.HtmlIdentityOperator.Instance;	
     }
 }
